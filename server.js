d32e29c2442bbaed17b1c82f4c9ee58c29c853e9
mimiMonads <mimi@monads.dev>
2025-03-23 20:07:30 +0000

    exporting app

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const messageRoutes = require("./backend/routes/messageRoutes");
 const userRoutes = require("./backend/routes/userRoutes");
 
+
+const app = express();
+
 // Load environment variables
-const env = require("dotenv").config();
+require("dotenv").config();
 
 // Middleware
 app.use(express.json());
@@ -14,7 +16,7 @@ app.use(cors());
 
 // Connect to MongoDB using .env variable
 mongoose
-  .connect(env.parsed["process.env.MONGO_URI"])
+  .connect(process.env.MONGO_URI) // Corrected to directly use process.env
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
@@ -22,8 +24,4 @@ mongoose
 app.use("/api/messages", messageRoutes);
 app.use("/", userRoutes);
 
-// Use PORT from .env or fallback
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
